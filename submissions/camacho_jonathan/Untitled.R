6c55e8d2aa887770b8f4c8d1d6103ed4feaca8ae
jonathan <jonathanecm@icloud.com>
2017-04-09 15:23:54 -0500

    Creating party variable

--- submissions/camacho_jonathan/Untitled.R
+++ submissions/camacho_jonathan/Untitled.R
@@ -4,7 +4,7 @@
 # Camacho Jonathan E.
 ###############################################################
 # This script accomplish the following tasks:
-  # Gets "nes" data. 
+# Gets "nes" data. 
 ###############################################################
 
 # Libraries necessary for the script to run. 
@@ -16,15 +16,13 @@ nes <- read_dta("data/nes2008.dta")
 
 # Prep biden related variables
 biden <- nes %>%
-  select(biden_therm_post, gender, partyid3, age, educ_r) %>% 
+  select(biden_therm_post, gender, partyid3, age, educ_r) %>%
   mutate_each(funs(ifelse(is.nan(.), NA, .))) %>%
-  mutate(gender = gender - 1,
-         dem = ifelse(partyid3 == 1, 1, 0),
-         rep = ifelse(partyid3 == 3, 1, 0)) %>%
-  mutate_each(funs(as.integer)) %>%
-  rename(biden = biden_therm_post,
+    mutate(gender = gender - 1, party = ifelse(partyid3 == 1, 1, ifelse(partyid3 == 3, 2, 0))) %>% 
+    mutate_each(funs(as.integer)) %>%
+    rename(biden = biden_therm_post,
          female = gender,
          educ = educ_r) %>%
   select(-partyid3) %>%
-  na.omit %>%
-  write_csv("data/biden.csv")
+  na.omit
+#   write_csv("data/biden.csv")
