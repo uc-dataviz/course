667b8da8702fe0a98aa37906a809fc3d78ec1634
jonathan <jonathanecm@icloud.com>
2017-05-09 14:46:14 -0500

    Adding data scripts

---
+++ submissions/camacho_jonathan/A3/runfile.R
@@ -0,0 +1,13 @@
+# Cleans existing files and directories and creates new directores.
+paths <- c("./data", "./graphics")
+
+for (path in paths) {
+    unlink(path, recursive = TRUE) 
+    dir.create(path)
+}
+
+source("00_download_data.R")
+source("01_clean_data.R")
+#source("02_exploratory_analysis.R")
+rmarkdown::render("02_report.Rmd", output_dir = "output")
+
